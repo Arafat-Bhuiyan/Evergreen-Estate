4d0fa3a8565e5c576c0245a7f0cca3dafdd660e8
Arafat Bhuiyan <arafatbhuiyan41@gmail.com>
2024-10-23 15:41:50 +0600

    home, footer, login, register, navbar pages added.

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../pages/Shared/Navbar/Navbar';
+import Footer from '../pages/Footer/Footer';
 
 const Root = () => {
     return (
         <div>
+            <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
