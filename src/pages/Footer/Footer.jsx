9696c85abf7182d038d6b9098b396b1bb1082541
Arafat Bhuiyan <arafatbhuiyan41@gmail.com>
2024-10-25 02:37:23 +0600

    footer added

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer bg-base-200 text-base-content p-10">
+      <aside>
+        <p className="text-3xl font-bold">Evergreen Estate Ltd.</p>
+        <p>
+          A contemporary theme we designed specifically for real estate and
+          property showcase websites, equipped with every option, element and
+          feature your site may need.
+        </p>
+      </aside>
+      <nav>
+        <h6 className="footer-title">Contact us</h6>
+        <a className="link link-hover">Extensitonn Pallabi, Mirpur, Dhaka, Bangladesh</a>
+        <a className="link link-hover">+880 1625693202</a>
+        <a className="link link-hover">+880 1933028624</a>
+        <a className="link link-hover">evergreenestate@gmail.com</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Categories</h6>
+        <a className="link link-hover">Recent property</a>
+        <a className="link link-hover">To Sell</a>
+        <a className="link link-hover">To Buy</a>
+        <a className="link link-hover">To Rent</a>
+      </nav>
 
-export default Footer
+      <nav>
+        <h6 className="footer-title">Legal</h6>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
+        <a className="link link-hover">FAQ Page</a>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
